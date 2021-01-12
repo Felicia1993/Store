360174bc1540f8beeff6e8a8c7716df85c1090c7
Felicia <359468250@qq.com>
2021-01-12 23:26:50 +0800

    整合redis，解决对外内存溢出

--- store-gateway/src/main/java/com/store/storegateway/StoreGatewayApplication.java
+++ store-gateway/src/main/java/com/store/storegateway/StoreGatewayApplication.java
@@ -2,8 +2,15 @@ package com.store.storegateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+/**
+ * 1.开启服务注册发现
+ * 2.配置nacos注册中心地址
+ */
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StoreGatewayApplication {
 
     public static void main(String[] args) {
--- store-product/src/main/java/com/store/storeproduct/service/impl/CategoryServiceImpl.java
+++ store-product/src/main/java/com/store/storeproduct/service/impl/CategoryServiceImpl.java
@@ -27,6 +27,21 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
 
         return new PageUtils(page);
     }
+    //TODO 产生堆外内存溢出：OutofDirectMemoryError
+    //1)springboot2.0以后默认使用lettuce作为操作redis的客户端，使用Netty进行网络通信，
+    //2)lettuce的bug导致对外内存溢出，netty如果没有指定对外内存，就会使用Xmx300m
+    //3)没有得到及时的内存释放，可以通过-Dio.netty.maxDirectMemory进行设置
+    //解决方案：不能使用-Dio.netty.maxDirectMemory只去调大对外内存
+    //1）升级 lettuce客户端 2）使用Jedis
+
+    /**
+     * redisTemplate:
+     * lettuce、jedis操作redis的底层。Spring对前两者再次封装成redisTemplate
+     *
+     * @param catelogId
+     * @return
+     */
+
     @Override
     public Long[] findCatelogPath(Long catelogId) {
 

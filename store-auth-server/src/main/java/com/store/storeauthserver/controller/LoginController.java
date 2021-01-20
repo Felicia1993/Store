43571771dea1a2b8530574a9db059f139c87b38f
Felicia <359468250@qq.com>
2021-01-20 22:31:26 +0800

    注册、登录页面整合

--- store-auth-server/src/main/java/com/store/storeauthserver/controller/LoginController.java
+++ store-auth-server/src/main/java/com/store/storeauthserver/controller/LoginController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
-    @GetMapping("/login.html")
+    /**
+     * 发送一个轻轻直接跳转到一个页面
+     * Springmvc viewcontroller：将轻轻和页面映射过滤
+     * @return
+     */
+    /*@GetMapping("/login.html")
     public String loginPage() {
         return "login";
     }
 
-    @GetMapping("/reg.html")
+    @GetMapping("/regin.html")
     public String regPage() {
-        return "reg";
-    }
+        return "regin";
+    }*/
 }
